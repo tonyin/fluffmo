83fd3e0d1d5c059522ff2e8f2e6c9dc481769c05
tony <tonyyin89@gmail.com>
2016-05-01 20:05:12 -0700

    add all of the comics

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__, instance_relative_config=True)
 
 # Load default config and then instance config
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 from app import views
--- app/views.py
+++ app/views.py
@@ -1,9 +1,24 @@
 from flask import render_template
 from app import app
 
+base_url = '/fluffymomo'
 
-@app.route('/')
-def index():
+
+@app.route(base_url + '/')
+@app.route(base_url + '/<int:comic_id>')
+def index(comic_id=1):
+    nav = {
+        'prev': True if comic_id > 1 else False,
+        'next': True if comic_id < 12 else False
+    }
+    comic = {
+        'img': 'no' + str(comic_id) + '.jpg',
+        'id': comic_id
+    }
+
     return render_template(
-        'index.html'
+        'index.html',
+        title='The Adventures of Fluffy & Momo',
+        nav=nav,
+        comic=comic
     )
